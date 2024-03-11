a50243397be1291b4bd7ac5ff0c9f82fb30d5d3b
Axel Carrillo <1722110634@utectulancingo.edu.mx>
2024-03-11 14:49:41 +0000

    app5

--- aplicacion_05/mvc/controllers/ver_producto.py
+++
@@ -1,8 +0,0 @@
-import web
-
-render = web.template.render('mvc/views/')
-
-class VerProducto:
-    def GET(self):
-        # Aquí podrías obtener el producto por su ID desde tu modelo
-        return render.ver_producto()
